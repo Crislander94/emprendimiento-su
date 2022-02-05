7996a482920272d6825f96c54c3ffab01cc4e4e7
Crislander94 <crhighlander94@gmail.com>
2022-02-05 12:36:21 -0500

    preparing proyecto

---
+++ config/settings.php
@@ -0,0 +1,13 @@
+<?php
+    //Ruta Total de la app
+    $ruta = $_SERVER['HTTP_HOST'];
+    $ruta_final = 'http://'.$ruta.'/multi-entrega-php-mysql-master/';
+    define('RUTA', $ruta_final);
+
+    //Definiendo Rutas por default de los controladores
+    define("CONTROLLER_MAIN", 'Producto');
+    define("ACTION_MAIN", 'index');
+    //Definiendo Rutas por default de los controladores
+    define("CONTROLLER_MAIN_ADMIN", 'Admin');
+    define("CONTROLLER_MAIN_REPARTIDOR", 'Repartidor');
+?>
